daa637bbf86212bc758400c471d96b2755c5f5b8
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-05-19 11:14:33 +0600

    Starting NodeJS Learning and created index & module file created!

---
+++ module.js
@@ -0,0 +1,12 @@
+// IIFE function
+// (function(exports, require, module, __dirname, __filename){
+//           var age = 24;
+//           // console.log(name);
+// })()//("Imdadul Haque")
+
+// NPM module (works in behind like above IIFE function)
+const color = require('cli-color');
+
+console.log(color.red("Hello NodeJS!"));
+
+
